6d387d3e54a3c1d00f220485cb59949be15355f2
pinekta <h03a081b@gmail.com>
2017-09-17 12:15:57 +0000

    Fix notice error

--- combineWavBySox.php
+++ combineWavBySox.php
@@ -2,13 +2,14 @@
 
 $inputFilePath = "dest/wav";
 $outputDir = "dest/combined";
+$prevNumber = null;
 
 try {
     exec("ls {$inputFilePath}", $inputFiles);
     natsort($inputFiles);
     foreach ($inputFiles as $input) {
         $articleNumber = substr($input, 0, strpos($input, '_'));
-        if ($articleNumber !== $prevNumber) {
+        if ($prevNumber !== null && $articleNumber !== $prevNumber) {
             $inputFiles = implode(" ", $inFiles);
             // ファイルを結合
             exec("sox {$inputFiles} {$outputDir}/{$prevNumber}.wav", $output);
